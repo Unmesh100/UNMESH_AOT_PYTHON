a36e734ff0595d7b166554904d1f8370dd4c6381
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2025-01-20 01:30:38 +0530

    Update Question_5.py

--- Assignment 2 ( Conditional and Loops )/Question_5.py
+++ Assignment 2 ( Conditional and Loops )/Question_5.py
@@ -11,3 +11,22 @@ else:
         factorial *= i  # Multiply each i with factorial
 
     print("Factorial of", number, "is", factorial)
+
+
+'''
+# With Recursion
+
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)  # Recursive call
+
+# Taking user input
+number = int(input("Enter a number to find its factorial: "))
+
+# Printing the result
+print(f"Factorial of {number} is {factorial(number)}")
+'''
