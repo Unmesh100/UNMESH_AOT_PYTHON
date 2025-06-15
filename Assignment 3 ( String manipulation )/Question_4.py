3535f9d483e5d59ab9f55001ce9d87fdb22157a5
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2025-06-15 21:01:09 +0530

    Update Question_4.py

--- Assignment 3 ( String manipulation )/Question_4.py
+++ Assignment 3 ( String manipulation )/Question_4.py
@@ -1,7 +1,7 @@
 s = input("Enter a string: ").lower()  # Convert input to lowercase
 reversed_s = s[::-1]  # Reverse the string
 
-# Check if the original string is the same as the reversed string
+# Check if the original string is the same as the reversed string 
 if s == reversed_s:
     print("The string is a palindrome.")
 else:
